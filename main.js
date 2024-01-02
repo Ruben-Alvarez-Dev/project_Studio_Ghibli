94f319c848b1166d5db0c03d748e29d7e3f4c864
ruben <ruben@iMacPro.home>
2024-01-02 15:30:45 +0100

    feature: fetch implementation

---
+++ main.js
@@ -0,0 +1,37 @@
+const urlBase = "https://ghibliapi.vercel.app";
+const urlEndpointFilms = "/films";
+const urlEndpointPeople = "/people";
+const urlEndpointLocations = "/locations";
+const urlEndpointSpecies = "/species";
+const urlEndpointVehicles = "/vehicles";
+
+const app = document.querySelector("#app");
+app.innerHTML = `<h2>Studio Ghibli API</h2>`;
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  mapData(data);
+  //return data;
+};
+
+const mapData = async (data) => {
+  const mappedData = data.map((film) => ({
+    title: film.title,
+    description: film.description,
+    poster: film.image,
+  }));
+
+  mappedData.forEach((element) => {
+    app.innerHTML += `
+    <div class="card">
+      <h3>${element.title}</h3>
+      <img src="${element.poster}" alt="${element.title}" />
+      <p>${element.description}</p>`;
+  });
+
+  console.log(mappedData);
+  // return mappedData;
+};
+
+getData(urlBase + urlEndpointFilms);
